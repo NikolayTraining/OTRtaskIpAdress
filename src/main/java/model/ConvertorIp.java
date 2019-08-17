b08965df0762c0d55a919a159867ffba3840ddcb
@NikolayTraining <ribal_kate@rambler.ru>
2019-08-17 14:18:59 +0300

    сделал процедуры работы с IP

--- src/main/java/model/ConvertorIp.java
+++ src/main/java/model/ConvertorIp.java
@@ -1,4 +1,23 @@
 package model;
 
 public class ConvertorIp {
+
+    public long ipToLong(String ip) {
+        String[] ipSplit = ip.split("\\.");
+        long longIp = 0;
+        for (int i = 0; i < ipSplit.length; i++) {
+            longIp += Integer.parseInt(ipSplit[i]) * Math.pow(256, 3 - i);
+        }
+        return longIp;
+    }
+
+    public String longToIp(long longIp) {
+        return ((longIp >> 24) & 0xFF) + "." +
+
+                ((longIp >> 16) & 0xFF) + "." +
+
+                ((longIp >> 8) & 0xFF) + "." +
+
+                (longIp & 0xFF);
+    }
 }
