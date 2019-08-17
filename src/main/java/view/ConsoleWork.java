f2af9c7da9a25b814ff8645d1f67660a7d3ca39d
@NikolayTraining <ribal_kate@rambler.ru>
2019-08-17 14:29:14 +0300

    процедуры ввода с клавиатуры IP и процедура валидности IP

--- src/main/java/view/ConsoleWork.java
+++ src/main/java/view/ConsoleWork.java
@@ -1,4 +1,62 @@
 package view;
 
+import model.ConvertorIp;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ConsoleWork {
+    private String messageConsole = null;
+    private String oneIp = null;
+    private String twoIp = null;
+    private String messageExit = "exit";
+    private long oneIpValue = 0;
+    private long twoIpValue = 0;
+
+
+    public boolean enterIpForConsole() {
+        messageConsole = null;
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (!messageExit.equalsIgnoreCase(messageConsole)) {
+                System.out.println("If you want to exit, enter: 'exit' ");
+                if (oneIp != null) {
+                    System.out.println("IP " + oneIp + " ~ Enter range IP address: ");
+                    messageConsole = scanner.nextLine();
+                } else {
+                    System.out.println("Enter range IP address: ");
+                    messageConsole = scanner.nextLine();
+                }
+                if (messageExit.equalsIgnoreCase(messageConsole)){
+                    return false;
+                }
+                if (checkForIp(messageConsole) == true) {
+                    if (oneIp != null) {
+                        twoIp = messageConsole;
+                        return true;
+                    } else {
+                        oneIp = messageConsole;
+                    }
+                    System.out.println("Ip address: " + messageConsole);
+                } else {
+                    System.out.println("Ip address is not correct!!!");
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return false;
+    }
+
+    public boolean checkForIp(String s) {
+        Pattern delimiterPattern = Pattern.compile("^(([1-9]?\\d|1\\d\\d|2[0-5][0-5]|2[0-4]\\d)\\.)" +
+                "{3}([1-9]?\\d|1\\d\\d|2[0-5][0-5]|2[0-4]\\d)$");
+        Matcher matcher = delimiterPattern.matcher(s);
+        if (matcher.matches()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
