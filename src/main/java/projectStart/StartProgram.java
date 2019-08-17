d7a53930eebe360dbb5bbfbaf961397cfa8a7a5c
@NikolayTraining <ribal_kate@rambler.ru>
2019-08-17 14:40:33 +0300

    реализация стартового метода

--- src/main/java/projectStart/StartProgram.java
+++ src/main/java/projectStart/StartProgram.java
@@ -1,4 +1,15 @@
 package projectStart;
 
+import view.ConsoleWork;
+import view.ConsoleWorkImpl;
+
 public class StartProgram {
+
+    public static void main(String[] args) {
+        ConsoleWorkImpl consoleWork = new ConsoleWork();
+        System.out.println("Hello!");
+        if ( consoleWork.enterIpForConsole()== true){
+            consoleWork.printRangeIp();}
+        System.out.println("You exit program.");
+    }
 }
